2c1ad578a4771e7c6eb3889b744121f381aef22d
Libing Chen <libing.chen@gmail.com>
2024-11-17 11:53:53 +0800

    app: AotMain

---
+++ src/main/java/org/mvnsearch/AotMain.java
@@ -0,0 +1,14 @@
+package org.mvnsearch;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AotMain {
+    public static void main(String... args) {
+        var words = List.of("hello", "fuzzy", "world");
+        var greeting = words.stream()
+                .filter(w -> !w.contains("z"))
+                .collect(Collectors.joining(", "));
+        System.out.println(greeting);  // hello, world
+    }
+}
