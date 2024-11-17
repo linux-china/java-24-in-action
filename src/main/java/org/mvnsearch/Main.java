ac1d288ea7414ac7e8e349df3b950715eaeb3e24
Libing Chen <libing.chen@gmail.com>
2024-11-17 11:54:23 +0800

    chore: rename to Main

--- src/main/java/org/mvnsearch/Main.java
+++ src/main/java/org/mvnsearch/Main.java
@@ -3,7 +3,7 @@ package org.mvnsearch;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class AotMain {
+public class Main {
     public static void main(String... args) {
         var words = List.of("hello", "fuzzy", "world");
         var greeting = words.stream()
